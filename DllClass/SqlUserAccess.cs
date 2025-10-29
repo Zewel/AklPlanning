4a6444358b7ae80b71a27165b4444131574c0b4e
Zillur <Zillur@ZEWEL>
2025-10-29 23:57:27 +0600

    commit

--- DllClass/SqlUserAccess.cs
+++ DllClass/SqlUserAccess.cs
@@ -6,13 +6,13 @@ namespace SweaterPlanning.DllClass
 {
     public static class SqlUserAccess
     {
-        //public static string DataSource = @"DESKTOP-V3H61Q6\SQL2016";
-        //public static string UserName = @"sa";
-        //public static string PassWord = @"sa1234";
+        public static string DataSource = @"ZEWEL";
+        public static string UserName = @"sa";
+        public static string PassWord = @"sa1234";
 
-        public static string DataSource = @"192.168.10.248";
-        public static string UserName = @"planning";
-        public static string PassWord = @"planning";
+        //public static string DataSource = @"192.168.10.248";
+        //public static string UserName = @"planning";
+        //public static string PassWord = @"planning";
 
 
     }
